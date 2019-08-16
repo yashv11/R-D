e79efccd3f0e9c6b85786ccc72b0e9215bf9c29c
yashv11 <54174592+yashv11@users.noreply.github.com>
2019-08-16 16:42:18 +0530

    Add files via upload

---
+++ Javascript/script.js
@@ -0,0 +1,10 @@
+document.getElementById("p1").innerHTML = "hiiiiiiiiiiiiiiiii";
+
+function paragraph(){
+var h = document.getElementById("para").innerHTML;
+alert(h);
+document.getElementById("para").innerHTML = "Hello Yash";
+document.getElementById("para").style.fontSize = "35px";
+
+}
+
